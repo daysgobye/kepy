ff011282f5b7e4f7590a93351dd2c8530b6bc996
Cole Smith <smithcole@protonmail.com>
2019-12-15 22:20:51 -0800

    indent

--- matrix.py
+++ matrix.py
@@ -8,10 +8,14 @@ def write_report(report):
     print (report.encode())
     with open('/dev/hidg0', 'rb+') as fd:
         fd.write(report.encode())
+
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(21, GPIO.OUT)
+
 matrix = keyMap.matrix
+
 COL = [5,6,13,19]
 ROW = [18,23,24,25]
 
@@ -28,19 +32,10 @@ while True:
         GPIO.output(COL[j],1)
         for i in range(4):
             if GPIO.input(ROW[i]) == 1:
-
-                #print ("col = ",(j+1))
-                #print ("row = ",(i+1))
-
                 m = ((i * 4) + j)
-                #print ("postion = ",(m))
-                #print (matrix [m])
-                #print (" ")
                 time.sleep(0.1)
-		#write_report(NULL_CHAR*2+chr(4)+NULL_CHAR*5)
-		write_report(NULL_CHAR*2+chr(matrix[m])+NULL_CHAR*5)
-		write_report(NULL_CHAR*8)
-		#write_report(chr(97))
+		        write_report(NULL_CHAR*2+chr(matrix[m])+NULL_CHAR*5)
+		        write_report(NULL_CHAR*8)
                 while(GPIO.input(ROW[i]) == 1):
                     GPIO.output(21,1)
                     time.sleep (0.2)
